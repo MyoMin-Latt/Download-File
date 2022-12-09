f389b360a2f98fe9eec8fe2b14094422b8413231
MyoMinLatt <myominlatt.sbs@gmail.com>
2022-12-09 23:05:45 +0630

    mml-imageList > download image, view, delete

--- lib/main.dart
+++ lib/main.dart
@@ -38,14 +38,24 @@ class _MyHomePageState extends State<MyHomePage> {
     Directory? directory = await getExternalStorageDirectory();
     String savePath = '${directory!.path}/Image/${url.split('/').last}';
     log('urlPath => $savePath');
-    await Dio()
-        .download(
-          url,
-          savePath,
-          onReceiveProgress: (count, total) =>
-              log('count/total => $count/$total'),
-        )
-        .then((value) => streamSink.add(savePath));
+    await Dio().download(
+      url,
+      savePath,
+      onReceiveProgress: (count, total) => log('count/total => $count/$total'),
+    );
+  }
+
+  Future<void> getDownloadedImage() async {
+    Directory? directory = await getExternalStorageDirectory();
+    Directory path = Directory('${directory!.path}/Image/');
+    List<FileSystemEntity> imageList = path.listSync();
+    streamSink.add(imageList);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getDownloadedImage();
   }
 
   @override
@@ -56,7 +66,8 @@ class _MyHomePageState extends State<MyHomePage> {
         actions: [
           IconButton(
               onPressed: () async {
-                await downloadImage(urlList[1]);
+                await downloadImage(urlList[6])
+                    .then((value) => getDownloadedImage());
               },
               icon: const Icon(Icons.download))
         ],
@@ -65,8 +76,45 @@ class _MyHomePageState extends State<MyHomePage> {
         child: StreamBuilder(
           stream: stream,
           builder: (context, snapshot) {
+            log(snapshot.data.toString());
             if (snapshot.hasData) {
-              return Image.file(File(snapshot.data.toString()));
+              return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) => Card(
+                  child: Stack(
+                    clipBehavior: Clip.none,
+                    children: [
+                      Image.file(
+                        File(snapshot.data![index].path),
+                        height: 200,
+                        width: double.infinity,
+                        fit: BoxFit.fill,
+                      ),
+                      Positioned(
+                        child: IconButton(
+                          onPressed: () {
+                            snapshot.data![index].delete();
+                            getDownloadedImage();
+                          },
+                          icon: const Icon(
+                            Icons.delete,
+                            color: Colors.red,
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        left: 2,
+                        bottom: 2,
+                        child: Container(
+                            color: Colors.black26,
+                            padding: const EdgeInsets.all(4),
+                            child: Text(
+                                snapshot.data![index].path.split('/').last)),
+                      ),
+                    ],
+                  ),
+                ),
+              );
             } else if (snapshot.hasError) {
               return const Text('Snapshot has Error');
             } else {
@@ -79,12 +127,16 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-StreamController<String> streamController = StreamController();
+StreamController<List<FileSystemEntity>> streamController = StreamController();
 StreamSink streamSink = streamController.sink;
-Stream<String> stream = streamController.stream;
+Stream<List<FileSystemEntity>> stream = streamController.stream;
 
 List<String> urlList = [
   'https://filesamples.com/samples/image/jpg/sample_1280%C3%97853.jpg',
   'https://sample-videos.com/img/Sample-jpg-image-50kb.jpg',
   'https://file-examples.com/storage/fe352586866388d59a8918d/2017/10/file_example_JPG_100kB.jpg',
+  'https://download.samplelib.com/jpeg/sample-clouds-400x300.jpg',
+  'https://download.samplelib.com/jpeg/sample-red-400x300.jpg',
+  'https://download.samplelib.com/jpeg/sample-green-400x300.jpg',
+  'https://download.samplelib.com/jpeg/sample-blue-400x300.jpg',
 ];
